4dc400e7bf6352a018fee305a915a557df9f9d91
hny <myslfabhi01@gmail.com>
2022-07-25 15:51:18 +0530

    test01

--- src/test/java/com/gitcmd1/First1stTest.java
+++ src/test/java/com/gitcmd1/First1stTest.java
@@ -4,7 +4,7 @@ public class First1stTest {
 
 	public static void main(String[] args) {
 		System.out.println("hiiiiiiiiiiifirst");
-		System.out.println("new one two three four");
+		System.out.println("new one two three four five");
 
 	}
 
